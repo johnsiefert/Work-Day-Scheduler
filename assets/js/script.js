9b4a16a000a0c91e3f802aa44dee5db8896c9656
johnsiefert <johnsief12@gmail.com>
2021-11-06 09:45:05 -0400

    added hour time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
     displayDate.innerHTML = NowMoment;
     let currentHour = moment().format("HH");
 
+//grabs hour from each time slot and compares it to actual time
+$(".time-div").each(function () {
+  var timeDiv = $(this).attr("id").split("-")[1];
+
     if (currentHour == timeDiv) {
         $(this).addClass("present");
         $(this).children(".description").addClass("white-text");
@@ -17,10 +21,22 @@ $(document).ready(function () {
       }
     });
 
-      //grabs values from time and value divs and saves them to local storage
+ //grabs values from time and value divs and saves them to local storage
   $(".saveBtn").click(function (event) {
     event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
+    let value = $(this).siblings(".time-block").val();
+    let time = $(this).parent().attr("id").split("-")[1];
     localStorage.setItem(time, value);
+  });
+
+    //retrieves items from local storage and sets them in proper places
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-11 .time-block").val(localStorage.getItem("11"));
+    $("#hour-12 .time-block").val(localStorage.getItem("12"));
+    $("#hour-13 .time-block").val(localStorage.getItem("13"));
+    $("#hour-14 .time-block").val(localStorage.getItem("14"));
+    $("#hour-15 .time-block").val(localStorage.getItem("15"));
+    $("#hour-16 .time-block").val(localStorage.getItem("16"));
+    $("#hour-17 .time-block").val(localStorage.getItem("17"));
   });
